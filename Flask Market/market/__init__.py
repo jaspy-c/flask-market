45b2376cf52ec44680d34af39215941367956c79
jaspy-c <142454012+jaspy-c@users.noreply.github.com>
2023-09-25 16:13:35 -0700

    Add files via upload

---
+++ Flask Market/market/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = os.getenv('secret_key')
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+app.app_context().push()
+
+from market import routes
